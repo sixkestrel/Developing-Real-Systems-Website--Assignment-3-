88e18e5558059e80fd238e7df00f96a24e958128
Harmony-D <162699330+Harmony-D@users.noreply.github.com>
2025-03-10 13:08:45 +0000

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>This is the header</header>
+    <main>This is the main</main>
+    <footer>This is the footer</footer>
+</body>
+</html>
